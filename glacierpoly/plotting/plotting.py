0e1a8363f3c7746fc2542fefa60cb18cffff56fc
Friedrich Knuth <knuth@uw.edu>
2021-08-17 16:15:39 -0700

    add candidate predictor variables

--- glacierpoly/plotting/plotting.py
+++ glacierpoly/plotting/plotting.py
@@ -7,31 +7,47 @@ import glob
 import os
 
 
-def plot_tif(tif_file, gdf, vlim = 10):
+def plot_tif(tif_file, 
+             glacier_outline_gdf=None,
+             cmap=None,
+             vmin=None,
+             vmax=None,
+             cbar_fraction=0.035):
+    
     source = rasterio.open(tif_file)
+    
+
 
     fig, ax = plt.subplots(figsize=(10, 10))
+    
+    sm = ax.imshow(source.read(1), 
+                         cmap=cmap, 
+                         vmin=vmin, 
+                         vmax=vmax)
 
     crs = cartopy.crs.epsg(source.crs.to_epsg())
 
     ax = plt.axes(projection=crs)
 
-    show(source,
-         cmap='RdBu',
-         vmin=-vlim,
-         vmax=vlim,
-         ax=ax,
-         interpolation='none')
+    show(source, cmap=cmap, ax=ax, interpolation="none",vmin=vmin,vmax=vmax)
 
-    gl = ax.gridlines(draw_labels=True,x_inline=False, y_inline=False)
+    gl = ax.gridlines(draw_labels=True, x_inline=False, y_inline=False)
 
-    gl.xlabel_style = {'size': 12, 'color': 'black', 'rotation': 0, 'rotation_mode': 'anchor'}
-    gl.ylabel_style = {'size': 12, 'color': 'black', 'rotation': 0, 'rotation_mode': 'anchor'}
+    gl.xlabel_style = {
+        "size": 12,
+        "color": "black",
+        "rotation": 0,
+        "rotation_mode": "anchor",
+    }
+    gl.ylabel_style = {
+        "size": 12,
+        "color": "black",
+        "rotation": 0,
+        "rotation_mode": "anchor",
+    }
     gl.right_labels = False
     gl.top_labels = False
 
-    sm = plt.cm.ScalarMappable(cmap='RdBu',norm=plt.Normalize(vmin=-vlim, vmax=vlim))
+    fig.colorbar(sm, ax=ax, fraction=cbar_fraction, pad=0.001, extend="both", aspect=50)
 
-    fig.colorbar(sm, ax=ax,fraction=0.035, pad=0.001, extend='both', aspect=50)
-    
-    gdf.plot(ax=ax, facecolor='none', edgecolor='b')
+    glacier_outline_gdf.plot(ax=ax, facecolor="none", edgecolor="b")
